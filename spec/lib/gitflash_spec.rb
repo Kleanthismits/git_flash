f083b08d14775d0f49d552d357d348388d83d38b
kmitsioulis <kleanthis.mitsioulis@persado.com>
2023-07-11 22:23:29 +0300

    temp

--- spec/lib/gitflash_spec.rb
+++ spec/lib/gitflash_spec.rb
@@ -1,4 +1,4 @@
-describe Gitflash do
+RSpec.describe Gitflash do
   it 'Zeiwerk works properly and eager loads all files' do
     expect { Zeitwerk::Loader.eager_load_all }.not_to raise_error
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,41 @@
 require 'simplecov'
+require 'simplecov-lcov'
+require 'gitflash'
 
-SimpleCov.start 'rails' do
-  add_filter 'spec/'
-  add_filter '.github/'
-  add_filter 'lib/generators/templates/'
-  add_filter 'lib/lokalise_rails/version'
-end
+# frozen_string_literal: true
 
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+RSpec.configure do |config|
+  # Enable flags like --only-failures and --next-failure
+  config.example_status_persistence_file_path = '.rspec_status'
+
+  # Disable RSpec exposing methods globally on `Module` and `main`
+  config.disable_monkey_patching!
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
 
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].sort.each { |f| require f }
 
-ENV['RAILS_ENV'] = 'test'
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start do
+  add_filter 'lib/gitflash/version.rb'
+end
+#  'rails' do
+#   add_filter 'spec/'
+#   add_filter '.github/'
+#   add_filter 'lib/generators/templates/'
+#   add_filter 'lib/lokalise_rails/version'
+# end
 
-require_relative '../spec/dummy/config/environment'
-ENV['RAILS_ROOT'] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
+# if ENV['CI'] == 'true'
+#   require 'codecov'
+#   SimpleCov.formatter = SimpleCov::Formatter::Codecov
+# end
+
+# ENV['RAILS_ENV'] = 'test'
+
+# require_relative '../spec/dummy/config/environment'
+# ENV['RAILS_ROOT'] ||= "#{File.dirname(__FILE__)}../../../spec/dummy"
