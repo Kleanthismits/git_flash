059f7ded08c3522be189c1f5a8a72da454b17355
kmitsioulis <kleanthis.mitsioulis@persado.com>
2023-11-04 20:21:36 +0200

    Supress zeitwerk warning

--- lib/gitflash.rb
+++ lib/gitflash.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
+# Suppress the zeitwerk warning for overriding :require method temporarily
+original_verbose = $VERBOSE
+$VERBOSE = nil
+
 require 'zeitwerk'
+require 'pry'
+
 loader = Zeitwerk::Loader.for_gem
-loader.setup # ready!
+loader.setup
+
+# Restore the original warning level
+$VERBOSE = original_verbose
 
 module Gitflash
 end
